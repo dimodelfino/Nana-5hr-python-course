5254fbdf737a029998565e90b923936806b3a18c
dimodelfino <dimodelfino@gmail.com>
2025-05-22 16:21:31 -0300

    test comment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from helper import validate_and_execute, user_input_message
 from math import ceil, floor
 
 user_input = ""
-
+''' Adding a comment to main '''
 
 while user_input != "exit":
     user_input = input(user_input_message)
